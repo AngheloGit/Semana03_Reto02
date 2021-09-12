c544619fb2a8bb8dbfe86ea85f6d2689c2f1edd5
Anghelo <SV43828468@idat.edu.pe>
2021-09-12 13:48:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// EL Geometrizador
+
+function calcCircumfrence(radio) { 
+    alert('La circunferencia es: ' + (2 * radio) * 3.1416);
+  }
+  
+  // calcCircumfrence(5); 
+
+
+  // La calculadora de la edad del cachorro
+
+  function calculateDogAge(canineAge, conversioRate) { 
+    alert('¡Tu perrito tiene ' +  (canineAge * conversioRate) + ' años en años de perro!');
+  }
+  
+  calculateDogAge(3, 7); 
+
+
+
+  
